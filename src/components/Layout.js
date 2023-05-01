3201075b6cfe1dd4984cdd801eb7fe2a5261a022
Michael Jo <michael.jo@gmail.com>
2023-04-30 17:17:01 -0700

    initialize

---
+++ src/components/Layout.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+const Layout = () => {
+  return (
+    <Fragment>
+      <Navbar />
+      <h1>Hello</h1>
+
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default Layout;
