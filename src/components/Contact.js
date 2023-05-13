94f1425e04c7f4bf2871e54307db5c5c1c3f5a8f
Michael Jo <michael.jo@gmail.com>
2023-05-13 11:54:57 -0700

    daisy ui hero works better, replace main with Landing

--- src/components/Contact.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Contact = () => {
-  return <h1>Contact form</h1>;
-};
-
-export default Contact;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,13 +3,17 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import Main from './Main';
 import About from './About';
+import Schedule from './Schedule';
+import Landing from './Landing';
 
 const Layout = () => {
   return (
     <Fragment>
       <Navbar />
+      <Landing />
       <Main />
       <About />
+      <Schedule />
       <Footer />
     </Fragment>
   );
