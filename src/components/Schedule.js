4f26c40b8626520ac5b16915d0d8b6531744a86e
Michael Jo <michael.jo@gmail.com>
2023-05-14 11:52:47 -0700

    initial layout completed

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -2,16 +2,35 @@ import React from 'react';
 
 const Schedule = () => {
   return (
-    <div className='hero h-48 bg-base-200'>
-      <div className='hero-content flex-row sm:flex-row'>
-        <div className='basis-1/2 min-h-min'>
-          <h1 className='text-base font-bold'>LANGLEY</h1>
-          <p className='py-6'>Monday: 7pm - 9pm</p>
-          <p className='py-6'>Wednesday: 7pm - 9pm</p>
+    <div className='container'>
+      <div className='flex-row grid grid-cols-2 gap-60'>
+        <div className='mx-9 my-8 min-h-min'>
+          <h1 className='text-2xl font-bold'>LANGLEY</h1>
+          <div className='text-lg leading-loose'>Monday: 7pm - 9pm</div>
+          <div className='text-lg leading-loose'>Wednesday: 7pm - 9pm</div>
         </div>
-        <div className='basis-1/2 min-h-min'>
-          <h1 className='text-base font-bold'>About Century Kumdo Club</h1>
-          <p className='py-6'>Friday: 7:30pm - 9pm</p>
+        <div className='mx-9 my-8 min-h-min'>
+          <h1 className='text-2xl font-bold'>COQUITLAM</h1>
+          <p className='text-lg leading-loose'>Friday: 7:30pm - 9pm</p>
+        </div>
+      </div>
+
+      <div className='flex-row grid grid-cols-2 gap-60'>
+        <div className='mx-9 mb-8 min-h-min'>
+          <h1 className='text-xl font-bold'>Address: </h1>
+          <div className='text-lg leading-loose'>
+            Lions Society West Langley Hall
+          </div>
+          <div className='text-lg leading-loose'>
+            9400 208 St, Langley Twp, BC V1M 2Y9
+          </div>
+        </div>
+        <div className='mx-9 mb-8 min-h-min'>
+          <h1 className='text-xl font-bold'>Address:</h1>
+          <div className='text-lg leading-loose'>Harbour View Elementary</div>
+          <div className='text-lg leading-loose'>
+            960 Lillian St, Coquitlam, BC V3J 5C7
+          </div>
         </div>
       </div>
     </div>
