68555508371d2133bb23d2c76dd9a5f3d8d1609d
Michael Jo <michael.jo@gmail.com>
2023-05-27 18:35:11 -0700

    gallery updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,27 +7,39 @@ const Main = () => {
         Gallery
       </div>
       <div className='carousel w-full'>
-        <div id='item1' className='carousel-item w-full'>
+        <div id='slide1' className='carousel-item relative w-full'>
           <img src='/sample1.jpg' className='w-full' />
+          <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
+            <a href='#slide4' className='btn btn-circle'>
+              ❮
+            </a>
+            <a href='#slide2' className='btn btn-circle'>
+              ❯
+            </a>
+          </div>
         </div>
-        <div id='item2' className='carousel-item w-full'>
+        <div id='slide2' className='carousel-item relative w-full'>
           <img src='/sample2.jpg' className='w-full' />
+          <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
+            <a href='#slide1' className='btn btn-circle'>
+              ❮
+            </a>
+            <a href='#slide3' className='btn btn-circle'>
+              ❯
+            </a>
+          </div>
         </div>
-        <div id='item3' className='carousel-item w-full'>
+        <div id='slide3' className='carousel-item relative w-full'>
           <img src='/sample3.jpg' className='w-full' />
+          <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
+            <a href='#slide2' className='btn btn-circle'>
+              ❮
+            </a>
+            <a href='#slide4' className='btn btn-circle'>
+              ❯
+            </a>
+          </div>
         </div>
-      </div>
-
-      <div className='flex justify-center w-full py-2 gap-2'>
-        <a href='#item1' className='btn btn-xs'>
-          1
-        </a>
-        <a href='#item2' className='btn btn-xs'>
-          2
-        </a>
-        <a href='#item3' className='btn btn-xs'>
-          3
-        </a>
       </div>
     </Fragment>
   );
