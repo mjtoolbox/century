dec1a9190c34b0882a72a8dce82257735b86a59c
Michael Jo <michael.jo@gmail.com>
2023-07-29 11:50:51 -0700

    metatag updated

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,18 +5,22 @@ export default function Document() {
     <Html lang='en' data-theme='corporate'>
       <Head>
         <meta charSet='UTF-8' />
+        <meta property='title' content='밴쿠버 한인 검도 클럽' />
         <meta
           name='description'
-          content='밴쿠버 한인 센츄리 검도 클럽 Century Kumdo Club - A Korean non-profit Kendo sports club'
+          content='캐나다 밴쿠버 유일의 한인 검도 클럽, 센츄리 검도 클럽입니다. 밴쿠버 한인 공동체 비영리 검도 스포츠 클럽입니다.'
         />
+        <meta property='url' content='https://www.centurykumdo.com' />
         <meta
           name='keywords'
           content='밴쿠버 한인 검도 클럽, 센츄리 검도 클럽, Kendo, Kumdo, Korean, Canada, Vancouver, Langley, Coquitlam, Greater Vancouver, 밴쿠버, 검도, 한인, 한국, 캐나다'
         />
         <meta name='author' content='Century Kumdo Club' />
-
-        {/* Korean language meta tags */}
         <meta property='og:title' content='밴쿠버 한인 검도 클럽' />
+        <meta
+          property='og:site_name'
+          content='센츄리 밴쿠버 한인 검도 클럽'
+        ></meta>
         <meta
           property='og:description'
           content='캐나다 밴쿠버 유일의 한인 검도 클럽, 센츄리 검도 클럽입니다. 밴쿠버 한인 공동체 비영리 검도 스포츠 클럽입니다.'
@@ -30,19 +34,6 @@ export default function Document() {
         <meta
           name='google-site-verification'
           content='N354zcCrBXtHpz6E-VePb0BP-T-eW-i0v_GqQyUPQzg'
-        />
-
-        {/* English language meta tags */}
-        <meta property='og:title' content='Century Kumdo Club' />
-        <meta
-          property='og:description'
-          content='밴쿠버 한인 검도 클럽 Century Kumdo Club - A Korean non-profit Kumdo sports club'
-        />
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://www.centurykumdo.com' />
-        <meta
-          property='og:image'
-          content='https://www.centurykumdo.com/logo.jpg'
         />
       </Head>
       <body>
