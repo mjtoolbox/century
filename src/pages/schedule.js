8139cd4fa086eec08dd55f9ab9ee23c2dca37c50
Michael Jo <michael.jo@gmail.com>
2023-08-20 20:46:19 -0700

    sep schedule updated

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -5,8 +5,11 @@ import { attributes } from '../content/home.md';
 const Schedule = () => {
   return (
     <div className='bg-base-100 py-8'>
-      <div className='flex h-screen w-full justify-center items-center'>
-        <img src='/082023.jpg' className=' max-w-xl ' />
+      <div className='flex h-auto w-full justify-center items-center'>
+        <img src='/082023.jpg' className=' max-w-sm' />
+      </div>
+      <div className='flex h-auto w-full justify-center items-center'>
+        <img src='/092023.jpg' className=' max-w-sm' />
       </div>
     </div>
   );
