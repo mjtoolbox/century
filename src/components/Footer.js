7c14864ed0118271312ce50143f304c803f4717b
Michael Jo <michael.jo@gmail.com>
2023-09-10 11:34:42 -0700

    instagram link updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -41,7 +41,7 @@ const Footer = () => {
       </div>
       <div>
         <div className='grid grid-flow-col gap-4'>
-          <a>
+          <a href='https://www.instagram.com/century.kumdo.club/' target='_blank'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               width='24'
