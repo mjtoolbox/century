2d517e857e293d11224058d0d48918198cc4a48e
Michael Jo <michael.jo@gmail.com>
2023-11-16 21:27:42 -0800

    overwrite title for other, holiday

--- src/pages/api/submit.js
+++ src/pages/api/submit.js
@@ -9,14 +9,17 @@ export default async function handler(req, res) {
   const query =
     'INSERT INTO event( title, detail, start_date, end_date, color, time_duration) VALUES ($1, $2, $3, $4, $5, $6) returning event_id';
   const values = [
-    data.title,
+    data.title === 'Other' || data.title === 'Holiday'
+      ? data.description
+      : data.title,
     data.description,
     data.date.substring(0, 10),
     data.date.substring(0, 10),
     data.color,
     data.time,
   ];
-  // console.log('query', query);
+
+  //console.log('query', query);
   // console.log('values', values);
 
   try {
