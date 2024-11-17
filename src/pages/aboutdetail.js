22ff9023681353d953dc7df652c1e5bbb6806ab5
Michael Jo <michael.jo@gmail.com>
2024-11-17 14:37:01 -0800

    aboutdetail cardlayout

--- src/pages/aboutdetail.js
+++ src/pages/aboutdetail.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '@/components/AppContext';
-import { useState, useEffect, useContext } from 'react';
 import { attributes } from '../content/home.md';
 
 const AboutDetail = () => {
-  const { language, setLanguage } = useContext(AppContext);
-
-  let { aboutDetail } = attributes;
+  const { language } = useContext(AppContext);
+  const { aboutDetail } = attributes;
 
   return (
-    <div className='bg-base-100 py-8'>
-      <h1 className='text-center text-2xl font-bold pb-5'>
+    <div className="bg-base-100 py-8">
+      {/* Title Section */}
+      <h1 className="text-center text-2xl font-bold pb-5 border-b border-gray-300">
         {language === 'en' ? aboutDetail.title : aboutDetail.ktitle}
       </h1>
-      <div className='container mx-auto '>
-        <div className='px-4'>
+
+      {/* Content Section */}
+      <div className="container mx-auto px-4 mt-6">
+        <div className="bg-gray-50 shadow-md rounded-lg p-6 leading-relaxed">
           {language === 'en'
             ? aboutDetail.description
             : aboutDetail.kdescription}
