690554907bfdaf8b752e8b347096cd6050a1f229
Michael Jo <michael.jo@gmail.com>
2024-11-17 14:32:29 -0800

    contactus updated

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -1,39 +1,37 @@
 import React from 'react';
-import {AppContext} from '@/components/AppContext';
+import { AppContext } from '@/components/AppContext';
 import { attributes } from '../content/home.md';
 
 const ContactUs = () => {
-  let { contactus } = attributes;
+  const { contactus } = attributes;
 
   return (
-    <div className='bg-base-100 py-8'>
-      <div className='text-3xl font-bold text-center m-10' id='gallery'>
-        {/* {language === 'en' ? props.schedule.title : props.schedule.ktitle} */}
+    <div className="bg-base-100 py-8 px-4">
+      <div className="text-3xl font-bold text-center mb-10" id="gallery">
         {contactus.title}
       </div>
-      <div className='flex justify-evenly flex-wrap' id='schedule'>
-        <div className='mx-9 my-8 min-h-min'>
-          <div className='card w-96 bg-base-100 shadow-xl'>
-            <div className='card-body'>
-              <h1 className='card-title font-bold'>LANGLEY</h1>
-              <div className='text-lg '>Monday: 7pm - 9pm</div>
-              <div className='text-lg '>Wednesday: 7pm - 9pm</div>
-              <h3 className='text-xl mt-3 '>Address: </h3>
-              <div className='text-lg '>{contactus.place2}</div>
-              <div className='text-lg '>{contactus.add2}</div>
-            </div>
+      <div className="flex flex-col md:flex-row justify-center md:justify-evenly flex-wrap gap-8" id="schedule">
+        {/* Langley Card */}
+        <div className="w-full md:w-96 bg-base-100 shadow-xl rounded-lg overflow-hidden">
+          <div className="card-body p-6">
+            <h1 className="card-title font-bold text-xl mb-2">LANGLEY</h1>
+            <div className="text-lg mb-1">Monday: 7pm - 9pm</div>
+            <div className="text-lg mb-3">Wednesday: 7pm - 9pm</div>
+            <h3 className="text-xl font-semibold mb-2">Address:</h3>
+            <div className="text-lg">{contactus.place2}</div>
+            <div className="text-lg">{contactus.add2}</div>
           </div>
         </div>
-        <div className='mx-9 my-8 min-h-min'>
-          <div className='card w-96 bg-base-100 shadow-xl'>
-            <div className='card-body'>
-              <h1 className='card-title font-bold'>COQUITLAM</h1>
-              <p className='text-lg '>Friday: 7pm - 9pm</p>
-              <p className='text-lg text-red-500'>{contactus.msg} </p>
-              <h3 className='text-xl mt-3 '>Address: </h3>
-              <div className='text-lg '>{contactus.place3}</div>
-              <div className='text-lg '>{contactus.add3}</div>
-            </div>
+
+        {/* Coquitlam Card */}
+        <div className="w-full md:w-96 bg-base-100 shadow-xl rounded-lg overflow-hidden">
+          <div className="card-body p-6">
+            <h1 className="card-title font-bold text-xl mb-2">COQUITLAM</h1>
+            <p className="text-lg mb-1">Friday: 7pm - 9pm</p>
+            <p className="text-lg text-red-500 mb-3">{contactus.msg}</p>
+            <h3 className="text-xl font-semibold mb-2">Address:</h3>
+            <div className="text-lg">{contactus.place3}</div>
+            <div className="text-lg">{contactus.add3}</div>
           </div>
         </div>
       </div>
